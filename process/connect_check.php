86528e2244180a05f501d662b3e7116f280c3fbe
VIKAS SHARMA <116336028+vickspanda@users.noreply.github.com>
2024-11-24 21:00:42 +0530

    final commit

--- process/connect_check.php
+++ process/connect_check.php
@@ -1,9 +1,9 @@
 <?php
 
-$DB_SERVER = 'localhost'; // e.g., 'localhost'
-$DB_USERNAME = 'your_user'; // e.g., 'postgres'
-$DB_PASSWORD = 'password';
-$DB_DATABASE = 'intercityexpress';
+$DB_SERVER = 'db'; // e.g., 'localhost'
+$DB_USERNAME = 'vickspanda_user'; // e.g., 'postgres'
+$DB_PASSWORD = 'f9bkm3PUogNzIS2MSQi5qW35j093X9IW';
+$DB_DATABASE = 'vickspanda';
 
 try {
     $conn = new PDO("pgsql:host=$DB_SERVER;dbname=$DB_DATABASE", $DB_USERNAME, $DB_PASSWORD);
