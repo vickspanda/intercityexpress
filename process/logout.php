4869d36e4d7b71aab313de8a1d3ace79a343fee7
VIKAS SHARMA <116336028+vickspanda@users.noreply.github.com>
2024-11-25 00:16:56 +0530

    final commit

--- process/logout.php
+++ process/logout.php
@@ -1,7 +1,7 @@
 <?php
+    session_start();
     echo "<html><head><title>Logging Out</title></head><html>";
     $user = $_GET["user"];
-    session_start();
     session_destroy();
     session_abort();
     echo '<script>window.location.href="'.$user.'index.html"</script>';
