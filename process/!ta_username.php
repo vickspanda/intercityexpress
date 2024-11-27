8857d1375650a358253138131334f536bdd6e7d8
VIKAS SHARMA <116336028+vickspanda@users.noreply.github.com>
2024-11-27 22:41:14 +0530

    final commit

--- process/!ta_username.php
+++ process/!ta_username.php
@@ -1,6 +1,9 @@
 <?php
-    if (!$ta_username) {
-        echo '<script>window.location.href="index.html"</script>';
-        exit;
-      }
+$query = "SELECT * FROM travel_agent WHERE username = $1;";
+$ta_array = array($ta_username);
+$result = pg_query_params($conn,$query,$ta_array);
+if (pg_num_rows($result) == 0) {
+    echo '<script>window.location.href="../travel_agent/index.html";</script>';
+    exit();
+}
 ?>
