e83f8a313d150d8d2712cfcc1e533ca9b87379db
VIKAS SHARMA <116336028+vickspanda@users.noreply.github.com>
2024-11-28 00:41:52 +0530

    final commit

--- process/view_ticket.php
+++ process/view_ticket.php
@@ -63,9 +63,12 @@
     include '../process/connect.php';
     include 'getUserStatus.php';
     if(isset($_SESSION['ticket_no'])){
-        $ticket_no = $_SESSION['ticket_no'];
-        if(isset($_SESSION['userType'])){
-            $userType = $_SESSION["userType"] ?? '';
+        if($_SESSION['bug'] == TRUE){
+            $ticket_no = $_SESSION['ticket_no'];
+            $_SESSION['bug'] = FALSE;
+            if(isset($_SESSION['userType'])){
+                $userType = $_SESSION["userType"] ?? '';
+            }
         }
     }
 
