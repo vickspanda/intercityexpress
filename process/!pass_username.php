3e69b819b09898763f0d44d000079052a04db6c9
VIKAS SHARMA <116336028+vickspanda@users.noreply.github.com>
2024-11-27 22:40:12 +0530

    final commit

--- process/!pass_username.php
+++ process/!pass_username.php
@@ -1,6 +1,9 @@
 <?php
-if (!$pass_username) {
-    echo '<script>window.location.href="index.html";</script>';
+$query = "SELECT * FROM passenger WHERE username = $1;";
+$pass_array = array($pass_username);
+$result = pg_query_params($conn,$query,$pass_array);
+if (pg_num_rows($result) == 0) {
+    echo '<script>window.location.href="../passenger/index.html";</script>';
     exit();
 }
 ?>
