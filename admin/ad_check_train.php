eab2c9020502785391d0117e12f4710d2e2a9e50
VIKAS SHARMA <116336028+vickspanda@users.noreply.github.com>
2024-11-27 21:53:11 +0530

    final commit

--- admin/ad_check_train.php
+++ admin/ad_check_train.php
@@ -1,9 +1,6 @@
 <?php
 
 session_start();
-$ad_username = $_SESSION["admin_username"] ?? '';
-include '../process/connect.php';
-include '../process/!admin_username.php';
 
 include '../process/connect_check.php';
 $pdo = $conn;
