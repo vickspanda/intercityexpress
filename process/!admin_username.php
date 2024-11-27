a8f10f37098b707d1cb60ff2d2b1140df6e1c9f5
VIKAS SHARMA <116336028+vickspanda@users.noreply.github.com>
2024-11-27 20:31:38 +0530

    final commit

--- process/!admin_username.php
+++ process/!admin_username.php
@@ -1,5 +1,7 @@
 <?php
-if (!isset($ad_username)) {
+$query = "SELECT * FROM admin WHERE username = $1;";
+$result = pg_query_params($conn,$query,$ad_username);
+if (pg_num_rows($ad_username) == 0) {
     echo '<script>window.location.href="index.html";</script>';
     exit();
 }
