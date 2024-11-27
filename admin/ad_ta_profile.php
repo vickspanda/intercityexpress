d70ff70c312ecef5e2108439ec03938bf9f4bbc7
VIKAS SHARMA <116336028+vickspanda@users.noreply.github.com>
2024-11-27 21:42:53 +0530

    final commit

--- admin/ad_ta_profile.php
+++ admin/ad_ta_profile.php
@@ -1,10 +1,10 @@
 <?php
 session_start();
 $ad_username = $_SESSION["admin_username"] ?? '';
-
+include '../process/connect.php';
 include '../process/!admin_username.php';
 
-include '../process/connect.php';
+
 
 
 if(isset($_GET["ta_username"]))
