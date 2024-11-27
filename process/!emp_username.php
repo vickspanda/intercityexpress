f2477965b7179c1bd3c45510c078f3a935133727
VIKAS SHARMA <116336028+vickspanda@users.noreply.github.com>
2024-11-27 22:38:59 +0530

    final commit

--- process/!emp_username.php
+++ process/!emp_username.php
@@ -1,6 +1,9 @@
 <?php
-    if (!$emp_username) {
-        echo '<script>window.location.href="index.html";</script>';
-        exit();
-    }
+$query = "SELECT * FROM employee WHERE username = $1;";
+$emp_array = array($emp_username);
+$result = pg_query_params($conn,$query,$emp_array);
+if (pg_num_rows($result) == 0) {
+    echo '<script>window.location.href="../employee/index.html";</script>';
+    exit();
+}
 ?>
